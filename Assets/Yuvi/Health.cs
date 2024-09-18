cfa88ce70eddcdbae2f5a40aa287427f6bee78a0
AbsoluteReality <89690272+AbsoluteReality@users.noreply.github.com>
2024-09-18 22:37:10 +0530

    Made Changes In Dialouge System, Interacting, Health and Energy Bar

--- Assets/Yuvi/Health.cs
+++ Assets/Yuvi/Health.cs
@@ -10,26 +10,62 @@ public class Health : MonoBehaviour
     // public Text healthText;
     public Image healthBar;
 
-    float lerpSpeed;
+    public Text playerkahealth;
+
+    public Text botkaenergyhealthbar;
+
+    public Image PlayerHealth;
+
+    float lerpSpeed = 5f;    
 
     float health;
+
+    float healthPlayer;
+
+    int botHealth;
+
     float maxHealth = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthPlayer = maxHealth;
     }
 
     // Update is called once per frame
-     void FixedUpdate()
+    void Update()
     {
         if (health > 0) {
-            
-            health -= 10 * Time.fixedDeltaTime;
+            health -= 10 * Time.deltaTime;
+            botHealth -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V) && healthPlayer > 0) {
+            healthPlayer -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.K) && healthPlayer < 100) {
+            healthPlayer += 10;
         }
 
         // healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed);
-        healthBar.fillAmount = health/100;
+        // healthBar.fillAmount = health/100;
+
+        if (health < 100 && Input.GetKeyDown(KeyCode.H)) {
+            health += 30;
+            botHealth += 10;
+            Debug.Log("Yo");
+            healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed * Time.deltaTime);   
+        }
+
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed * Time.deltaTime);   
+        // healthBar.fillAmount = health/100;
+        PlayerHealth.fillAmount = Mathf.Lerp(PlayerHealth.fillAmount, healthPlayer/100, lerpSpeed * Time.deltaTime);
+        
+
+        
+        playerkahealth.text = healthPlayer.ToString();
+        // botkaenergyhealthbar.text = botHealth.ToString();
+
     }
 }
