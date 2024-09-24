09133e665957809f8124739c47272b3f5d19c713
KBrijeshReddy <brijeshreddy12@gmail.com>
2024-09-24 23:37:35 +0530

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBHHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIRRRRRRRRRRRRRRRRRAAAAAAAAAAAAAAAAMMMMMMMMMMMMMMMMMMM

--- Assets/Scripts/Bots/Health.cs
+++ Assets/Scripts/Bots/Health.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
+    public static Health instance; void Awake() { instance = this; }
 
     // public Text healthText;
     public Image healthBar;
 
-    public Text playerkahealth;
-
-    public Text botkaenergyhealthbar;
-
-
+    public TMP_Text playerkahealth;
 
     public Image PlayerHealth;
 
-    public float damage = 30;
+    public float damage;
     public float healthnipenchurakojja = 30;
 
-    float lerpSpeed = 5f;
+    float lerpSpeed = 5f;    
 
     float health;
 
@@ -30,6 +28,8 @@ public class Health : MonoBehaviour
     int botHealth;
 
     float maxHealth = 100;
+
+    public bool takingDmg = false;
 
     // Start is called before the first frame update
     void Start()
@@ -42,51 +42,46 @@ public class Health : MonoBehaviour
     void Update()
     {
 
-        if (health > 0)
-        {
+        if (health > 0) {
             health -= 10 * Time.deltaTime;
             botHealth -= 10;
         }
 
-        if (Input.GetKeyDown(KeyCode.V) && healthPlayer > 0)
-        {
-            if (healthPlayer <= damage)
-            {
+        if (takingDmg && healthPlayer > 0) {
+            if (healthPlayer <= damage) {
                 healthPlayer = 0;
+                takingDmg = false;
             }
-            if (healthPlayer > damage)
-            {
+            else if (healthPlayer > damage) {
                 healthPlayer -= damage;
+                takingDmg = false;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.K) && healthPlayer < 100 && (100 - healthPlayer) <= healthnipenchurakojja)
-        {
+        if (Input.GetKeyDown(KeyCode.K) && healthPlayer < 100 && (100 - healthPlayer) <= healthnipenchurakojja) {
             healthPlayer = 100;
         }
 
-        if (Input.GetKeyDown(KeyCode.K) && healthPlayer < 100 && (100 - healthPlayer) > healthnipenchurakojja)
-        {
+        if (Input.GetKeyDown(KeyCode.K) && healthPlayer < 100 && (100 - healthPlayer) > healthnipenchurakojja) {
             healthPlayer += healthnipenchurakojja;
         }
 
         // healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed);
         // healthBar.fillAmount = health/100;
 
-        if (health < 100 && Input.GetKeyDown(KeyCode.H))
-        {
+        if (health < 100 && Input.GetKeyDown(KeyCode.H)) {
             health += 30;
             botHealth += 10;
             Debug.Log("Yo");
-            healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / 100, lerpSpeed * Time.deltaTime);
+            healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed * Time.deltaTime);   
         }
 
-        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / 100, lerpSpeed * Time.deltaTime);
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health/100, lerpSpeed * Time.deltaTime);   
         // healthBar.fillAmount = health/100;
-        PlayerHealth.fillAmount = Mathf.Lerp(PlayerHealth.fillAmount, healthPlayer / 100, lerpSpeed * Time.deltaTime);
+        PlayerHealth.fillAmount = Mathf.Lerp(PlayerHealth.fillAmount, healthPlayer/100, lerpSpeed * Time.deltaTime);
+        
 
-
-
+        
         playerkahealth.text = healthPlayer.ToString();
         // botkaenergyhealthbar.text = botHealth.ToString();
 
